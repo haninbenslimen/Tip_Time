74ff25881878eb543963a97e22f5b116f63383ad
Hanine Benslimene <haninebenslimene@gmail.com>
2021-10-30 01:02:28 +0100

    last commit

--- app/src/main/java/com/haninetoumaima/tiptime/MainActivity.kt
+++ app/src/main/java/com/haninetoumaima/tiptime/MainActivity.kt
@@ -1,42 +1,86 @@
 package com.haninetoumaima.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.haninetoumaima.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{ calculateTip() }
 
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
-    fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+
+    private fun calculateTip() {
+
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            binding.tipResult.text = ""
+
+
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
             return
         }
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+
         var tip = tipPercentage * cost
+
+
         val roundUp = binding.roundUpSwitch.isChecked
         if (roundUp) {
+
             tip = kotlin.math.ceil(tip)
         }
-        NumberFormat.getCurrencyInstance()
+
+
+        displayTip(tip)
+    }
+
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
 
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
